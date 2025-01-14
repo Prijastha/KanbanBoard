607c803f1d30bd25e6e2f86b80284994849478b0
Prija <shresthaprija123@gmail.com>
2025-01-14 19:11:06 +0545

    kanbanboard

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import { Column, Id, Task } from "../types";
 import ColumnContainer from "./ColumnContainer";
